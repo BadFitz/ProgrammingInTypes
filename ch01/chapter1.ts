884877dd882f82d81bb3c877e6d938ece3940507
BackPockets <dffuller+backpockets@gmail.com>
2020-02-16 13:11:14 -0600

    new check-in

---
+++ ch01/chapter1.ts
@@ -0,0 +1,11 @@
+function booleanToString(b: boolean): string {
+	if (b) {
+		return "true";
+	} else {
+		return "false";
+	}
+}
+
+
+console.log(booleanToString(true));
+console.log(booleanToString(false));
